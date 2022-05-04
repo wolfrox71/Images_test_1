1b9bb4df2bb405ba25c0ed54c125d50eb75dfcdf
wolfr <wolfr@DESKTOP-I8FG5VK>
2022-05-04 11:54:36 +0200

    Add project files.

---
+++ Images_test_1/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Images_test_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            imageLogo.Source = new BitmapImage(new Uri(@"/Resources/slow-worms-eyes.jpg",UriKind.Relative));
+        }
+    }
+}
